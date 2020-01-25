95cb98a9bf55b68bc8c040ba49e79b1a0f7d69c3
Jimmy <jimmy.gense@lacatholille.fr>
2020-01-25 19:36:55 +0100

    component to functional component and begining login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 import Booking from './Booking/Booking';
 import Login from "./Login/Login";
+import UserPanel from "./UserPanel/UserPanel";
+
+import './App.css'
 
 export default class App extends React.Component {
 
@@ -14,12 +22,19 @@ export default class App extends React.Component {
         return (
             <div className={"App"}>
                 <Router>
-                    <Link to={"/"}>Accueil</Link>
-                    <Link to={"/booking"}>Booking</Link>
+                    <AppBar position={"static"}>
+                        <Toolbar>
+
+                            <Button><Link className={"menu-button"} to={"/"}>Accueil</Link></Button>
+                            <Button><Link className={"menu-button"} to={"/booking"}>Booking</Link></Button>
+                            <Button><Link className={"menu-button"} to={"/account"}>Account</Link></Button>
+                        </Toolbar>
+                    </AppBar>
 
                     <Switch>
                         <Route exact path={"/"}><Login/></Route>
                         <Route exact path={"/booking"}><Booking/></Route>
+                        <Route exact path={"/account"}><UserPanel/></Route>
                     </Switch>
                 </Router>
             </div>
